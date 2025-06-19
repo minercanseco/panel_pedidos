d9139ad3c9ffe7de835662f16ef4f3d813709d9c
MinerCanseco <miner_canseco@hotmail.com>
2025-06-19 13:52:49 -0600

    commit inicial

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     ventana_login = ttk.Window()
     parametros_login = ParametrosContpaqi()
 
-    actualizar = ActualizadorDePaquetes('panel_pedidos_v45')
+    actualizar = ActualizadorDePaquetes('panel_pedidos_v46')
     if not actualizar.actualizar_paquete():
         # ------------------------------------------
         # parametros de prueba

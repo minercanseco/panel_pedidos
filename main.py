9ead43b8b93128fe344e1fc5c08225a8c0a7ac52
MinerCanseco <miner_canseco@hotmail.com>
2025-06-19 16:19:35 -0600

    commit inicial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import ttkbootstrap as ttk
-from ttkbootstrap.dialogs import Messagebox
+import platform
 
+import ttkbootstrap as ttk
 from cayal.parametros_contpaqi import ParametrosContpaqi
 from interfaz_panel_pedidos import InterfazPanelPedidos
 from modelo_panel_pedidos import ModeloPanelPedidos
@@ -9,11 +9,7 @@ from cayal.actualizador_de_paquetes import ActualizadorDePaquetes
 from cayal.login import Login
 
 
-def mostrar_mensaje(mensaje, titulo="Actualización"):
-    Messagebox.show_info(title=titulo, message=mensaje, alert=True)
-
 def si_acceso_exitoso(parametros=None, master=None):
-    #xmaster.grab_release()
     llamar_instancia_principal(master, parametros)
 
 def llamar_instancia_principal(ventana, parametros):
@@ -26,21 +22,23 @@ if __name__ == '__main__':
     ventana_login = ttk.Window()
     parametros_login = ParametrosContpaqi()
 
-    actualizar = ActualizadorDePaquetes('panel_pedidos_v48')
+    actualizador = ActualizadorDePaquetes('panel_pedidos_v48')
 
-    if actualizar.actualizar_paquete():
-        mostrar_mensaje("Se actualizará el paquete, por favor espere.")
-        ventana_login.update_idletasks()
-        ventana_login.withdraw()  # Oculta la ventana sin destruirla
-        actualizar.actualizar_paquete()
-        ventana_login.destroy()
+    if actualizador.actualizar_paquete():
+        actualizador.actualizar_con_interfaz(ventana_login, on_terminado=ventana_login.destroy)
 
-    if not actualizar.actualizar_paquete():
+    if not actualizador.actualizar_paquete():
+        # valida si es en mac o windows
+        if platform.system() == 'Darwin':
+            # parametros de prueba
+            parametros_login.cadena_conexion = 'Mac'
+        else:
+            # parametros de produccion
+            parametros_login.base_de_datos = 'ComercialSP'
+
         # ------------------------------------------
-        # parametros de prueba
-        parametros_login.cadena_conexion = 'Mac'
-        #parametros_login.base_de_datos = 'ComercialSP'
+        # modulo de pedidos
         parametros_login.id_modulo = 1687
-        # ------------------------------------------
+
         instancia_login = Login(ventana_login, parametros_login, si_acceso_exitoso)
         ventana_login.mainloop()

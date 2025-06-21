091dd2a93965ca03f7a54f76841d7f410b660e26
MinerCanseco <miner_canseco@hotmail.com>
2025-06-21 10:06:55 -0600

    commit inicial

--- controlador_captura.py
+++ controlador_captura.py
@@ -60,7 +60,7 @@ class ControladorCaptura:
 
         self._rellenar_desde_base_de_datos()
 
-        #self._master.lift()  # Traer al frente
+        self._master.lift()  # Traer al frente
         #self._master.focus_force()  # Forzar el foco
 
         self._configurar_pedido()

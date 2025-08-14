8237adc77f4ddc6bb2e9594ee4956af3abd6b3d7
MinerCanseco <miner_canseco@hotmail.com>
2025-08-14 10:44:27 -0600

    actualizaciones

--- interfaz_captura.py
+++ interfaz_captura.py
@@ -23,7 +23,6 @@ class InterfazCaptura:
         self._agregar_validaciones()
         self._cargar_captura_manual()
 
-        self.ventanas.centrar_ventana_ttkbootstrap(self.master)
 
     def _cargar_frames(self):
         nombre_frame_anuncio = 'Anuncios' if self.modulo_id not in [1687] else 'Captura manual'

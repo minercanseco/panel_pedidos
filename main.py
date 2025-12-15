826001b12c1884d988da57142b740be69fbcb4c2
MinerCanseco <miner_canseco@hotmail.com>
2025-12-14 23:30:49 -0600

    actualizaciones

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     ventana_login = ttk.Window()
     parametros_login = ParametrosContpaqi()
 
-    version_paquete = 'panel_pedidos_v115'
+    version_paquete = 'panel_pedidos_v116'
     parametros_login.version_paquete = version_paquete
 
     actualizador = ActualizadorDePaquetes(version_paquete)

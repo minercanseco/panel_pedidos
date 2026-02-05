c47cf037ffb62ba30642689eff867853a62dd192
MinerCanseco <miner_canseco@hotmail.com>
2026-02-05 13:54:37 -0600

    inicializacion del proyecto

--- herramientas/capturar_documento/buscar_generales_cliente.py
+++ herramientas/capturar_documento/buscar_generales_cliente.py
@@ -513,7 +513,7 @@ class BuscarGeneralesCliente:
         # 4) cargar la informacion de las direcciones del cliente
         self.cliente.addresses_details = []
         direcciones_cliente = self._buscar_info_direcciones_cliente_seleccionado(business_entity_id)
-        print(direcciones_cliente)
+
         for direccion in direcciones_cliente:
             self.cliente.add_address_detail(direccion)
         self.cliente.addresses_details = direcciones_cliente

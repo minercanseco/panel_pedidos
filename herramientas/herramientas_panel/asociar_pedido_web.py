0ee6b9043f47c467a2b53b8b0cf4559c0450a9fb
MinerCanseco <miner_canseco@hotmail.com>
2026-05-13 12:28:52 -0600

    agregar modulo de asociacion de pedidos cliente no definido

--- herramientas/herramientas_panel/asociar_pedido_web.py
+++ herramientas/herramientas_panel/asociar_pedido_web.py
@@ -10,4 +10,23 @@ class AsociarPedidoWeb:
         self._ventanas = Ventanas(self._master)
 
         self._info_pedido = info_pedido
+        self._crear_componentes()
+
         self._ventanas.configurar_ventana_ttkbootstrap()
+
+
+    def _crear_componentes(self):
+        componentes = [
+            ('cbx_accion', 'Acción:'),
+            ('tbv_historial', self._crear_columnas_tabla()),
+        ]
+
+        self._ventanas.crear_formulario_simple(componentes)
+
+    def _crear_columnas_tabla(self):
+        return [
+            {"text": "BusinessEntityID", "stretch": False, "width": 0},
+            {"text": "Cliente", "stretch": False, "width": 350},
+            {"text": "Teléfono", "stretch": False, "width": 120},
+            {"text": 'Correo', "stretch": False, "width": 170},
+        ]
